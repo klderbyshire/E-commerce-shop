fa7d72292aabcbc6412462126510c1cbdf644204
Elena Sparshott <65815679+ElenaSparshott@users.noreply.github.com>
2022-06-29 14:07:01 +0100

    product cards

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,19 +1,26 @@
 import React from 'react';
+import { useState, useEffect} from 'react';
 import './App.css';
+import Cards from '../Cards/cards.js'; 
 
 function App() {
 
-  const getData = async () => {
+    const [data, setData] = useState()
+
+    useEffect(() => {
+    const getData = async () => {
     const res = await fetch(`https://fakestoreapi.com/products`)
     const data = await res.json()
+    setData(data)
     console.log(data)
   }
   getData()
-
+   }, []);
 
   return (
     <div className="App">
       <h1>Shop!</h1>
+      <Cards/>
     </div>
   );
 }
