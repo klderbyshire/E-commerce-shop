2ac0796ccdb6cf0fa73420ad9a133327db396721
karen derbyshire <karenderbyshire5@gmail.com>
2022-07-01 11:22:34 +0100

    basket made

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,36 +1,57 @@
-import React from 'react';
-import { useState, useEffect} from 'react';
-import './App.css';
-import Cards from '../Cards/cards.js'; 
+import React from "react";
+import { useState, useEffect } from "react";
+import "./App.css";
+import Cards from "../Cards/cards.js";
+import Drawer from "@mui/material/Drawer";
+import Basket from "../Basket/basket";
 
 function App() {
+  const [data, setData] = useState();
+  const [basket, setBasket] = useState([]);
+  const [cart, setCart] = useState();
 
-    const [data, setData] = useState()
-
-    useEffect(() => {
+  useEffect(() => {
     const getData = async () => {
-    const res = await fetch(`https://fakestoreapi.com/products`)
-    const data = await res.json()
-    setData(data)
-    console.log(data)
+      const res = await fetch(`https://fakestoreapi.com/products`);
+      const data = await res.json();
+      setData(data);
+      console.log(data);
+    };
+    getData();
+  }, []);
+
+  function addToBasket(id) {
+    let foundItem = data.find((item) => {
+      if (item.id === id) {
+        return item;
+      }
+    });
+    setBasket((basket) => [...basket, foundItem]);
   }
-  getData()
-   }, []);
+  console.log(basket);
 
   return (
     <div className="App">
-      {data ? data.map((item) => {
-        return (
-         <Cards
-         key={item.id}
-         id={item.id}
-         image={item.image}
-         title={item.title}
-         price={item.price}
-         />
-        )
-      })
-       : null}
+      <h3>My Shop!</h3>
+      <Drawer anchor="right" open={cart} onClose={() => setCart(false)}>
+        Your Shopping Cart!
+        <Basket basket={basket} />
+      </Drawer>
+      <button onClick={() => setCart(true)}>Open Cart</button>
+      {data
+        ? data.map((item) => {
+            return (
+              <Cards
+                key={item.id}
+                id={item.id}
+                image={item.image}
+                title={item.title}
+                price={item.price}
+                addToBasket={addToBasket}
+              />
+            );
+          })
+        : null}
     </div>
   );
 }
