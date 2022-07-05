d0a93cac6c2e806abcecb75a632c14ece0c6c9b2
Elena Sparshott <65815679+ElenaSparshott@users.noreply.github.com>
2022-07-05 23:52:16 +0100

    css

--- src/Components/Cards/cards.js
+++ src/Components/Cards/cards.js
@@ -7,7 +7,7 @@ function Cards({title, image, price, id, addToBasket}) {
         <h3>{title}</h3>
         <img className='product-images' src={image} alt={title}/>
         <h4>£{price}</h4>
-        <button onClick={() => addToBasket(id)}>Add to basket</button>
+        <button className="cardbutton" onClick={() => addToBasket(id)}>Add to basket</button>
     </div>
   )
 }
