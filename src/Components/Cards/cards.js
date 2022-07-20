b9427f4aa4884ad4d32042064dccc97cf3b14cb0
karen derbyshire <karenderbyshire5@gmail.com>
2022-07-20 13:49:08 +0100

    Add to basket button added

--- src/Components/Cards/cards.js
+++ src/Components/Cards/cards.js
@@ -1,12 +1,12 @@
 import React from 'react'
 
-function Cards({title, image, price, id}) {
+function Cards({title, image, price, id, addToBasket}) {
     return (
     <div className='cards'>
         <h3>{title}</h3>
         <img className='product-images' src={image} alt={title}/>
         <h4>£{price}</h4>
-        {/* <button onClick={() => addToBasket(id)}>Add to basket</button> */}
+        <button onClick={() => addToBasket(id)}>Add to basket</button>
     </div>
   )
 }
